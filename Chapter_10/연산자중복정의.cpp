a10bd6933664f3c77554dd80aaff82ee27d17b41
KimJuYoung <ymail3@naver.com>
2022-06-12 11:36:02 +0900

    연산자중복정의
    연산자중복정의

---
+++ Chapter_10/연산자중복정의.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+class MyVector {
+private:
+	double x, y;
+public:
+	MyVector(double x = 0.0, double y = 0.0) {
+		this->x = x, this->y = y;
+	}
+	string toString() {
+		return "(" + to_string(x) + ", " + to_string(y) + ")";
+	}
+	MyVector operator+(const MyVector& v2);
+};
+
+MyVector MyVector::operator+(const MyVector& v2) {
+	MyVector v;
+	v.x = this->x + v2.x;
+	v.y = this->y + v2.y;
+	return v;
+}
+
+int main()
+{
+	MyVector v1(1.0, 2.0), v2(3.0, 4.0);
+	MyVector v3 = v1 + v2;
+	cout << v1.toString() << " + " << v2.toString() << " = " << v3.toString() << endl;
+	return 0;
+}
